6b8ecf28c2be6d12685ee8c40545fca91f1458e4
mehrad <mmfirst@protonmail.com>
2024-10-16 12:35:21 +0330

    temp styling

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,30 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#eee";
 
 export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
+    // light: {
+    //     text: "#000",
+    //     background: "#fff",
+    //     secondary: "#222",
+    //     tint: tintColorLight,
+    //     tabIconDefault: "#ccc",
+    //     tabIconSelected: tintColorLight,
+    // },
+    light: {
+        text: "#fff",
+        background: "#000",
+        secondary: "rgba(255,255,255,0.125)",
+        tint: tintColorDark,
+        tabIconDefault: "#ccc",
+        tabIconSelected: tintColorDark,
+    },
+    dark: {
+        text: "#fff",
+        background: "#151515",
+        secondary: "#44444422",
+        // secondary: "#1b193bcc",
+        tint: tintColorDark,
+        tabIconDefault: "#ccc",
+        tabIconSelected: tintColorDark,
+    },
 };
